68f801654f613fff5b018d91ce2748e3740eeb89
Ekunkoya <oluwaseun.ekunkoya@dynatrace.com>
2024-07-24 16:07:50 +0200

    flutter train

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                // curve the corners a bit
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(25),
+            child: Text(
+              "EKUN GANGAN",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
